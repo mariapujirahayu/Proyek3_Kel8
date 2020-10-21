b11d077492e2125165ddf192d88bf5016742de76
mariapujirahayu <mariapujir@gmail.com>
2020-10-21 21:59:19 +0700

    ubah bootstrap

--- Admin/hapusdatanilai.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('koneksi.php');
-$sql = "DELETE FROM nilai_mahasiswa WHERE id_nilai='$_GET[id]'";
-if ($koneksi->query($sql) === TRUE) {
-    echo "<script>alert('HAPUS berhasil');window.location='datanilai.php';</script>";
-} else {
-    echo "Error: " . $sql . "<br>" . $koneksi->error;
-}
-?>
--- Admin/koneksi.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-$koneksi = new mysqli("localhost", "root", "", "penentuanpkl");
-
-?>
